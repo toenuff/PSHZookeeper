5866d5bed8089af7b2d429346d1ad88d0def97fc
Tome Tanasovski <toenuff@gmail.com>
2015-08-18 17:08:40 -0400

    added zookeeper module and connect-zookeeper function

---
+++ zookeeper.psm1
@@ -0,0 +1,156 @@
+$zookeeperdll = ([zookeepernet.zookeeper]).assembly.location
+$log4netdll = ([log4net.LogManager]).assembly.location
+
+$code = @"
+namespace ZooKeeperNet.watcher
+{
+    using System;
+    using System.Runtime.CompilerServices;
+    using System.Threading;
+
+	public class Watcher : IWatcher
+	{
+		public delegate void ChangedEvent(object sender, WatchedEvent e);
+		public event ChangedEvent Changed;
+
+		[MethodImpl(MethodImplOptions.Synchronized)]
+		public virtual void Process(WatchedEvent @event)
+		{
+			if (Changed != null) {
+				Changed(this, @event);
+			}
+		}
+	}
+}
+"@
+add-type -typedefinition $code -referencedassemblies $zookeeperdll, $log4netdll
+
+function New-EphemeralNode {
+	param(
+		  [Parameter(Mandatory=$true, ValueFromPipeline=$true)]
+		  [Alias('zkclient')]
+		  [ZookeeperNet.Zookeeper] $InputObject,
+		  [Parameter(Mandatory=$true, Position=0)]
+		  [string] $Path,
+		  [Parameter(Mandatory=$false, Position=1)]
+		  [string] $InputText = 0 #defaults to the byte value of zero if there is no data
+	)
+	write-verbose 'New-EphemeralNode'
+	$InputObject.create($path, [byte[]][char[]]$InputText, [ZooKeeperNet.Ids]::OPEN_ACL_UNSAFE, [zookeepernet.createmode]::Ephemeral)
+}
+
+function Update-NodeData {
+	param(
+		  [Parameter(Mandatory=$true, ValueFromPipeline=$true)]
+		  [Alias('zkclient')]
+		  [ZookeeperNet.Zookeeper] $InputObject,
+		  [Parameter(Mandatory=$true, Position=0)]
+		  [string] $Path,
+		  [Parameter(Mandatory=$false, Position=1)]
+		  [string] $InputText = 0 #defaults to the byte value of zero if there is no data
+	)
+	$InputObject.SetData($path, [byte[]][char[]]$InputText, -1)
+}
+
+function New-SequentialNodeData {
+	param(
+		  [Parameter(Mandatory=$true, ValueFromPipeline=$true)]
+		  [Alias('zkclient')]
+		  [ZookeeperNet.Zookeeper] $InputObject,
+		  [Parameter(Mandatory=$true, Position=0)]
+		  [string] $Path,
+		  [Parameter(Mandatory=$false, Position=1)]
+		  [string] $InputText = 0 #defaults to the byte value of zero if there is no data
+	)
+	Write-Verbose "Creating item in $path"
+	$return = $InputObject.Create($path, [byte[]][char[]]$InputText, [zookeeperNet.Ids]::OPEN_ACL_UNSAFE, [zookeepernet.createmode]::PERSISTENTSequential)
+	$return
+}
+
+function New-PersistentDir {
+	param(
+		  [Parameter(Mandatory=$true, ValueFromPipeline=$true)]
+		  [Alias('zkclient')]
+		  [ZookeeperNet.Zookeeper] $InputObject,
+		  [Parameter(Mandatory=$true, Position=0)]
+		  [string] $Path
+	)
+	try {
+		Write-Verbose "Create $path"
+		$return = $InputObject.create($path, 0, [ZookeeperNet.Ids]::OPEN_ACL_UNSAFE, [zookeepernet.createmode]::PERSISTENT)
+		Write-Verbose $return
+	} catch [ZooKeeperNet.KeeperException+NodeExistsException] {
+		Write-Verbose "$path already exists - skipped"
+	}
+}
+
+function Connect-Zookeeper {
+    param(
+          [string[]] $Computername = @("127.0.0.1:2181"),
+          [System.Timespan] $Timeout = (New-Timespan -Seconds 10),
+          [Parameter(Mandatory=$true)]
+          [scriptblock] $Action
+          
+    )
+    # action should return the message "Completed" on a line for it to execute
+    # and end.  Otherwise, connect-zookeeper will run in a loop and attempt to reconnect
+    # when it is disconnected.
+
+    $servers = $computername -join ','
+
+    $SCRIPT:connectionjob = $null
+    $SCRIPT:zkclient = $null
+
+    $finished = $false
+    while (!$finished) {
+        if (!$SCRIPT:zkclient) {
+            write-verbose "connecting to $servers"
+            $connectionwatcher = new-object zookeepernet.watcher.watcher
+            $actionstart = @'
+    $message = $event.sourceargs |select state, type, path
+    write-verbose "Connection Watcher Triggered"
+    switch ($message.state[-1]) {
+        'SyncConnected' {
+            Write-verbose "Connection to Zookeeper established"
+
+'@
+            $actionend = @'
+
+            break
+        }
+        $null {
+            break
+        }
+        default {
+            "RestartZKCLient"
+        }
+    }
+'@
+            $scriptblock = [scriptblock]::create($actionstart + $action.tostring() + $actionend)
+            $SCRIPT:connectionjob = Register-ObjectEvent -InputObject $connectionwatcher -EventName Changed -Action $scriptblock
+            $SCRIPT:zkclient = new-object ZooKeeperNet.ZooKeeper -ArgumentList @($servers, $timeout, $connectionwatcher)
+        }
+        sleep 5
+        $SCRIPT:connectionjob |receive-job -norecurse |% {
+            write-verbose $_
+            switch ($_) {
+                "RestartZKCLient" {
+                    $SCRIPT:zkclient.dispose()
+                    $SCRIPT:zkclient = $null
+                    get-job |stop-job -passthru |remove-job
+                    break
+                }
+                "Completed" {
+                    $SCRIPT:zkclient.dispose()
+                    $SCRIPT:zkclient = $null
+                    $finished = $true
+                    $SCRIPT:connectionjob |stop-job -passthru |remove-job
+                    break
+                }
+                default {
+                    $_
+                }
+            }
+        }
+    }
+}
