2780aa12566f517aa4ff95ce7aeb590c41c56cf2
Tome Tanasovski <toenuff@gmail.com>
2015-08-18 21:49:28 -0400

    Worker code ported to use the new zookeeper module

--- examples/worker.ps1
+++ examples/worker.ps1
@@ -1,30 +1,26 @@
 [cmdletbinding()]
 param(
-	  [string] $Name='worker1',
+	  [string] $Name='master1',
 	  [string[]] $Computername = @("127.0.0.1:2181")
 )
 
+$currdir = ''
+if ($MyInvocation.MyCommand.Path) {
+    $currdir = Split-Path $MyInvocation.MyCommand.Path
+} else {
+    $currdir = $pwd -replace '^\S+::',''
+}
+import-module (join-path $currdir ..\zookeeper.psd1) -force -verbose
+
 $servers = $computername -join ','
 
-# dot sourcing functions til this is proper module
-. .\module.ps1
-
-if (!(Test-path $name)) {
-	mkdir $name |out-null
+$workerdir = join-path $currdir $name
+if (!(Test-path $workerdir)) {
+	mkdir $workerdir| out-null
 }
 
-# Following is for dev purpose - ensure a blank zkclient
-if ($zkclient) {
-	$zkclient.dispose()
-}
-$zkclient = $null
-get-job |stop-job -passthru |remove-job
 
-$timeout = New-Timespan -seconds 10
-$connectionwatcher = $null
-$connectionjob = $null
-
-function Start-Worker {
+function GLOBAL:Start-Worker {
 	param(
 		  [Parameter(Mandatory=$true, ValueFromPipeline=$true)]
 		  [Alias('zkclient')]
@@ -33,46 +29,7 @@ function Start-Worker {
 	$InputObject |New-EphemeralNode -Path "/workers/$name"
 }
 
-function New-ConnectionWatcher {
-	# this function stinks - I'd much wrather create the objects and pass zkclient to it, but zkclient doesn't exist the first time it is created.
-	# That's the reason for all of the hoakie code here.  If I tried to use $sender.zkclient it might not exist by the time I need it and I would have no way
-	# of waiting for it to show up because it will have already triggered.  The only way around would be to update the c# class to have a default constructor
-	# and a connect() method to make the actual connection.
-	# I also can't put this in the module code because of scoping issues - I need access to the global zkclient
-	$GLOBAL:connectionwatcher = new-object zookeepernet.watcher.watcher
-	$GLOBAL:connectionjob = Register-ObjectEvent -InputObject $connectionwatcher -EventName Changed -Action {
-		$message = $event.sourceargs |select state, type, path
-		write-verbose "Connection Watcher Triggered"
-		switch ($message.state[-1]) {
-			'SyncConnected' {
-				Write-verbose "Starting Node"
-				$zkclient |Start-Worker
-				break
-			}
-			$null {
-				break
-			}
-			default {
-				"RestartZKCLient"
-			}
-		}
-	}
+Connect-Zookeeper -ComputerName $servers -Action {
+    Write-verbose "Attempting to grab a lock for master"
+    $GLOBAL:zkclient |Start-Worker
 }
-
-while ($true) {
-	if (!$zkclient) {
-		write-verbose "connecting to $servers"
-		New-ConnectionWatcher
-		$zkclient = new-object ZooKeeperNet.ZooKeeper -ArgumentList @($servers, $timeout, $GLOBAL:connectionwatcher)
-	}
-	sleep 5
-	$GLOBAL:connectionjob |receive-job -norecurse |% {
-		write-verbose $_
-		if ($_ -eq "RestartZKCLient") {
-			$zkclient.dispose()
-			$zkclient = $null
-			get-job |stop-job -passthru |remove-job
-		}
-	}
-}
-
